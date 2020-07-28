1f8ee60fcdeae424814d4e0e989400803241f023
Zan Zibert <zan_zibert@hotmail.com>
2020-07-28 11:28:41 +0200

    session utility function

---
+++ ChitChat/util.go
@@ -0,0 +1,15 @@
+import (
+	"errors"
+	"net/http"
+)
+
+func session(w http.ResponseWriter, r *http.Request) (sess data.Session, err error) {
+	cookie, err := r.Cookie("_cookie")
+	if err == nil {
+		ses = data.Session{Uuid: cookie.Value}
+		if ok, _ := sess.Check(); !ok {
+			err = errors.New("Invalid session")
+		}
+	}
+	return
+}
