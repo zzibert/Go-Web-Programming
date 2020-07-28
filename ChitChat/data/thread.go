994b8a964d9fda007cda283af50c331368746174
Zan Zibert <zan_zibert@hotmail.com>
2020-07-28 15:11:34 +0200

    the threads function

--- ChitChat/data/thread.go
+++ ChitChat/data/thread.go
@@ -9,3 +9,19 @@ type Thread struct {
 	UserId    int
 	CreatedAt time.Time
 }
+
+func Threads() (threads []Thread, err error) {
+	rows, err := Db.Query("SELECT id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		th := Thread{}
+		if err = rows.Scan(&th.Id, &th.Uuid, &th.Topic, &th.UserId, &th.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, th)
+	}
+	rows.Close()
+	return
+}
