817af55d91e608f04b9b32359e82c2ac8c8d1317
Zan Zibert <zan_zibert@hotmail.com>
2020-07-28 15:52:46 +0200

    NumReplies method

--- ChitChat/data/thread.go
+++ ChitChat/data/thread.go
@@ -25,3 +25,17 @@ func Threads() (threads []Thread, err error) {
 	rows.Close()
 	return
 }
+
+func (thread *Thread) NumReplies() (count int) {
+	rows, err := Db.Query("SELECT count(*) FROM posts where thread_id = $1", thread.Id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		if err = rows.Scan(&count); err != nil {
+			return
+		}
+	}
+	rows.Close()
+	return
+}
