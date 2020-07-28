8bbb4d4464543d2cf6bd09914c1654f7149536b0
Zan Zibert <zan_zibert@hotmail.com>
2020-07-28 12:58:34 +0200

    the final index handler function

--- ChitChat/main.go
+++ ChitChat/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"text/template"
 )
 
 func main() {
@@ -38,23 +37,10 @@ func index(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		_, err := session(w, r)
 
-		public_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/public.navbar.html",
-			"templates/index.html",
-		}
-		private_tmpl_files := []string{
-			"templates/layout.html",
-			"templates/private.navbar.html",
-			"templates/index.html",
-		}
-
-		var templates *template.Template
 		if err != nil {
-			templates = template.Must(template.ParseFiles(public_tmpl_files...))
+			generateHTML(w, threads, "layout", "public.navbar", "index")
 		} else {
-			templates = template.Must(template.ParseFiles(private_tmpl_files...))
+			generateHTML(w, threads, "layout", "private.navbar", "index")
 		}
-		templates.ExecuteTemplate(w, "layout, threads")
 	}
 }
