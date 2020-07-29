4dc7b4b5c86ebcd7caefc1b406277c744516d9de
Zan Zibert <zan_zibert@hotmail.com>
2020-07-29 12:46:06 +0200

    chaining handlers

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -3,29 +3,28 @@ package main
 import (
   "fmt"
   "net/http"
-  "reflect"
-  "runtime"
 )
 
-func hello(w http.ResponseWriter, r *http.Request) {
+type HelloHandler struct{}
+
+func (h HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello!")
 }
 
-func log(h http.HandlerFunc) http.HandlerFunc {
-  return func(w http.ResponseWriter, r *http.Request) {
-    name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
-    fmt.Println("Handler function called - " + name)
-    h(w, r)
-  }
+func log(h http.Handler) http.Handler {
+  return http.HandlerFunc (func(w http.ResponseWriter, r *http.Request) {
+    fmt.Printf("Handler called - %T\n", h)
+    h.ServeHTTP(w, r)
+  })
 }
 
 func main() {
 
-
+  hello := HelloHandler{}
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.HandleFunc("/hello", log(hello))
+  http.Handle("/hello", log(hello))
 
   server.ListenAndServe()
 }
