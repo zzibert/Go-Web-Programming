1566d0a09704f1cd4efaafa35220515e91475617
Zan Zibert <zan_zibert@hotmail.com>
2020-07-29 12:18:55 +0200

    chaining two handler functions

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -3,14 +3,20 @@ package main
 import (
   "fmt"
   "net/http"
+  "reflect"
+  "runtime"
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello!")
 }
 
-func world(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "World!")
+func log(h http.HandlerFunc) http.HandlerFunc {
+  return func(w http.ResponseWriter, r *http.Request) {
+    name := runtime.FuncForPC(reflect.ValueOf(h).Pointer()).Name()
+    fmt.Println("Handler function called - " + name)
+    h(w, r)
+  }
 }
 
 func main() {
@@ -19,8 +25,7 @@ func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.HandleFunc("/hello", hello)
-  http.HandleFunc("/world", world)
+  http.HandleFunc("/hello", log(hello))
 
   server.ListenAndServe()
 }
