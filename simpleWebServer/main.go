c759759e18cccc1ef16201c896c8f70ad086196b
Zan Zibert <zan_zibert@hotmail.com>
2020-07-29 11:49:20 +0200

    handling requests with multiple handlers

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -5,18 +5,27 @@ import (
   "net/http"
 )
 
-type MyHandler struct{}
+type HelloHandler struct{}
 
-func (h *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Hello Wrold!")
+func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello!")
+}
+
+type WorldHandler struct{}
+
+func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "World!")
 }
 
 func main() {
 
-  handler := MyHandler{}
+  hello := HelloHandler{}
+  world := WorldHandler{}
   server := http.Server{
     Addr: "127.0.0.1:8080",
-    Handler: &handler,
   }
+  http.Handle("/hello", &hello)
+  http.Handle("/world", &world)
+  
   server.ListenAndServe()
 }
