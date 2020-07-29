6c904513dbddcb34e20f3f944bb3088cfa95ec9d
Zan Zibert <zan_zibert@hotmail.com>
2020-07-29 11:54:48 +0200

    handling requests with handler functions

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -5,27 +5,22 @@ import (
   "net/http"
 )
 
-type HelloHandler struct{}
-
-func (h *HelloHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func hello(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello!")
 }
 
-type WorldHandler struct{}
-
-func (h *WorldHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func world(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "World!")
 }
 
 func main() {
 
-  hello := HelloHandler{}
-  world := WorldHandler{}
+
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.Handle("/hello", &hello)
-  http.Handle("/world", &world)
-  
+  http.HandleFunc("/hello", hello)
+  http.HandleFunc("/world", world)
+
   server.ListenAndServe()
 }
