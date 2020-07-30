926892f21d39ac16e97071289c4ada66fdcb34b4
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 11:13:06 +0200

    reading headers from a request

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -2,21 +2,18 @@ package main
 
 import (
   "fmt"
-  "github.com/julienschmidt/httprouter"
   "net/http"
 )
 
-func hello(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-  fmt.Fprintf(w, "hello, %s!\n", p.ByName("name"))
-}
+func headers(w http.ResponseWriter, r *http.Request) {
+  h := r.Header
+  fmt.Fprintln(w, h)
+} 
 
 func main() {
-  mux := httprouter.New()
-  mux.GET("/hello/:name", hello)
-
   server := http.Server{
     Addr: "127.0.0.1:8080",
-    Handler: mux,
   }
+  http.HandleFunc("/headers", headers)
   server.ListenAndServe()
 }
