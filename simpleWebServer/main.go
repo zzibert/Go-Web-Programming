c7a3b6cc3796b86ce3d2a38103764e053b619068
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 12:23:39 +0200

    parsing forms

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -5,17 +5,16 @@ import (
   "net/http"
 )
 
-func body(w http.ResponseWriter, r *http.Request) {
-  len := r.ContentLength
-  body := make([]byte, len)
-  r.Body.Read(body)
-  fmt.Fprintln(w, string(body))
+func process(w http.ResponseWriter, r *http.Request) {
+  r.ParseForm()
+  fmt.Fprintln(w, r.Form)
 }
 
 func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.HandleFunc("/body", body)
+  http.HandleFunc("/process", process)
   server.ListenAndServe()
 }
+
