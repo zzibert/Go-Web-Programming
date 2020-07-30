759b3e87e4f6a4cb1876f1bd02d4646d59a3dfca
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 18:02:09 +0200

    iterator action

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -3,14 +3,12 @@ package main
 import (
   "net/http"
   "html/template"
-  "math/rand"
-  "time"
 )
 
 func process(w http.ResponseWriter, r *http.Request) {
   t, _ := template.ParseFiles("tmpl.html")
-  rand.Seed(time.Now().Unix())
-  t.Execute(w, rand.Intn(10) > 5)
+  daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
+  t.Execute(w, daysOfWeek)
 }
 
 func main() {
