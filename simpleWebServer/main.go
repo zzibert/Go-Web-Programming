5a2ecae7c855962a2782ad6ecd072738e7f673f4
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 14:41:43 +0200

    write to send responses to the client

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -1,20 +1,21 @@
 package main
 
 import (
-  "fmt"
   "net/http"
 )
 
-func process(w http.ResponseWriter, r *http.Request) {
-  r.ParseForm()
-  fmt.Fprintln(w, r.Form)
+func writeExample(w http.ResponseWriter, r *http.Request) {
+  str := `<html>
+  <head><title>Go Web Programming</title></head> <body><h1>Hello World</h1></body>
+  </html>`
+  w.Write([]byte(str))
 }
 
 func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.HandleFunc("/process", process)
+  http.HandleFunc("/write", writeExample)
   server.ListenAndServe()
 }
 
