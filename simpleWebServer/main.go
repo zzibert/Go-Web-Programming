bb0b85382ace220763cf3aabaff49aa8e6575a79
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 16:03:25 +0200

    getting cookies from the header

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "fmt"
   "net/http"
 )
 
@@ -19,11 +20,17 @@ func setCookie(w http.ResponseWriter, r *http.Request){
   http.SetCookie(w, &c2)
 }
 
+func getCookie(w http.ResponseWriter, r *http.Request) {
+  h := r.Header["Cookie"]
+  fmt.Fprintln(w, h)
+}
+
 func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
   http.HandleFunc("/set_cookie", setCookie)
+  http.HandleFunc("/get_cookie", getCookie)
   server.ListenAndServe()
 }
 
