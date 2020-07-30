ce626327ddac5e7978f240b97c17a64fce1ae0e4
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 17:28:46 +0200

    triggering a template engine from a handler function

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -1,45 +1,20 @@
 package main
 
 import (
-  "encoding/base64"
-  "fmt"
   "net/http"
-  "time"
+  "html/template"
 )
 
-func setMessage(w http.ResponseWriter, r *http.Request) {
-  msg := []byte("Hello World!")
-  c := http.Cookie{
-    Name: "flash",
-    Value: base64.URLEncoding.EncodeToString(msg),
-  }
-  http.SetCookie(w, &c)
-}
-
-func showMessage(w http.ResponseWriter, r *http.Request) {
-  c ,err := r.Cookie("flash")
-  if err != nil {
-    if err == http.ErrNoCookie {
-      fmt.Fprintln(w, "No message found")
-    }
-  } else {
-    rc := http.Cookie{
-      Name: "flash",
-      MaxAge: -1,
-      Expires: time.Unix(1, 0),
-    }
-    http.SetCookie(w, &rc)
-    val, _ := base64.URLEncoding.DecodeString(c.Value)
-    fmt.Fprintln(w, string(val))
-  }
+func process(w http.ResponseWriter, r *http.Request) {
+  t, _ := template.ParseFiles("tmpl.html")
+  t.Execute(w, "Hello World!")
 }
 
 func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.HandleFunc("/set_message", setMessage)
-  http.HandleFunc("/show_message", showMessage)
+  http.HandleFunc("/process", process)
   server.ListenAndServe()
 }
 
