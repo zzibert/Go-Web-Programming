9fc9255b1f347eb13f9edf5da6d23d159f549b70
Zan Zibert <zan_zibert@hotmail.com>
2020-07-30 16:26:33 +0200

    implementing flash messages using Go cookies

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -1,42 +1,45 @@
 package main
 
 import (
+  "encoding/base64"
   "fmt"
   "net/http"
+  "time"
 )
 
-func setCookie(w http.ResponseWriter, r *http.Request){
-  c1 := http.Cookie{
-    Name: "first_cookie",
-    Value: "Go Web Programming",
-    HttpOnly: true,
+func setMessage(w http.ResponseWriter, r *http.Request) {
+  msg := []byte("Hello World!")
+  c := http.Cookie{
+    Name: "flash",
+    Value: base64.URLEncoding.EncodeToString(msg),
   }
-  c2 := http.Cookie{
-    Name: "second_cookie",
-    Value: "Manning Publications Co",
-    HttpOnly: true,
-  }
-  http.SetCookie(w, &c1)
-  http.SetCookie(w, &c2)
+  http.SetCookie(w, &c)
 }
 
-func getCookie(w http.ResponseWriter, r *http.Request) {
-  c1, err := r.Cookie("first_cookie")
+func showMessage(w http.ResponseWriter, r *http.Request) {
+  c ,err := r.Cookie("flash")
   if err != nil {
-    fmt.Fprintln(w, "Cannot get the first cookie")
+    if err == http.ErrNoCookie {
+      fmt.Fprintln(w, "No message found")
+    }
+  } else {
+    rc := http.Cookie{
+      Name: "flash",
+      MaxAge: -1,
+      Expires: time.Unix(1, 0),
+    }
+    http.SetCookie(w, &rc)
+    val, _ := base64.URLEncoding.DecodeString(c.Value)
+    fmt.Fprintln(w, string(val))
   }
-  cs := r.Cookies()
-
-  fmt.Fprintln(w, c1)
-  fmt.Fprintln(w, cs)
 }
 
 func main() {
   server := http.Server{
     Addr: "127.0.0.1:8080",
   }
-  http.HandleFunc("/set_cookie", setCookie)
-  http.HandleFunc("/get_cookie", getCookie)
+  http.HandleFunc("/set_message", setMessage)
+  http.HandleFunc("/show_message", showMessage)
   server.ListenAndServe()
 }
 
