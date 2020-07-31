e3f8908d2ff07b092af9e8103e78f9aacc09891a
Zan Zibert <zan_zibert@hotmail.com>
2020-07-31 13:11:32 +0200

    setting the dot

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -7,8 +7,7 @@ import (
 
 func process(w http.ResponseWriter, r *http.Request) {
   t, _ := template.ParseFiles("tmpl.html")
-  daysOfWeek := []string{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"}
-  t.Execute(w, daysOfWeek)
+  t.Execute(w, "hello")
 }
 
 func main() {
