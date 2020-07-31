8e1cc4db026c87734cec0e2c28e35872ce5fcf13
Zan Zibert <zan_zibert@hotmail.com>
2020-07-31 15:45:41 +0200

    custom function for templates

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -3,11 +3,19 @@ package main
 import (
   "net/http"
   "html/template"
+  "time"
 )
 
+func formatDate(t time.Time) string {
+  layout := "2006-01-02"
+  return t.Format(layout)
+}
+
 func process(w http.ResponseWriter, r *http.Request) {
-  t, _ := template.ParseFiles("t1.html", "t2.html")
-  t.Execute(w, "hello world!")
+  funcMap := template.FuncMap { "fdate": formatDate }
+  t := template.New("tmpl.html").Funcs(funcMap)
+  t, _ = t.ParseFiles("tmpl.html")
+  t.Execute(w, time.Now())
 }
 
 func main() {
