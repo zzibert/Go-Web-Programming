a04fac577ec87ad3bb6d17eefe86d357b3ffc180
Zan Zibert <zan_zibert@hotmail.com>
2020-08-01 14:23:50 +0200

    reading and writing CSV

--- simpleWebServer/main.go
+++ simpleWebServer/main.go
@@ -1,31 +1,66 @@
 package main
 
+
 import (
+  "encoding/csv"
   "fmt"
-  "io/ioutil"
   "os"
+  "strconv"
 )
 
+type Post struct {
+  Id int
+  Content string
+  Author string
+}
+
 func main() {
-  data := []byte("Hello World!\n")
-  err := ioutil.WriteFile("data1", data, 0644)
+  csvFile, err := os.Create("posts.csv")
   if err != nil {
     panic(err)
   }
-  read1, _ := ioutil.ReadFile("data1")
-  fmt.Print(string(read1))
+  defer csvFile.Close()
 
-  file1, _ := os.Create("data2")
-  defer file1.Close()
+  allPosts := []Post{
+    Post{Id: 1, Content: "Hello World!", Author: "Sau Sheong"},
+    Post{Id: 2, Content: "Bonjour Monde!", Author: "Pierre"},
+    Post{Id: 3, Content: "Hola Mundo!", Author: "Pedro"},
+    Post{Id: 4, Content: "Greetings Earthlings!", Author: "Sau Sheong"},
+  }
 
-  bytes, _ := file1.Write(data)
-  fmt.Printf("Wrote %d bytes to file\n", bytes)
+  writer := csv.NewWriter(csvFile)
+  for _, post := range allPosts {
+    line := []string{strconv.Itoa(post.Id), post.Content, post.Author}
+    err := writer.Write(line)
+    if err != nil {
+      panic(err)
+    }
+  }
+  writer.Flush()
 
-  file2, _ := os.Open("data2")
-  defer file2.Close()
+  file, err := os.Open("posts.csv")
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
 
-  read2 := make([]byte, len(data))
-  bytes, _ = file2.Read(read2)
-  fmt.Printf("Read %d bytes from file\n", bytes)
-  fmt.Println(string(read2))
+  reader := csv.NewReader(file)
+  reader.FieldsPerRecord = -1
+  record, err := reader.ReadAll()
+
+  if err != nil {
+    panic(err)
+  }
+
+  var posts []Post
+  for _, item := range record {
+    id, _ := strconv.ParseInt(item[0], 0, 0)
+    post := Post{Id: int(id), Content: item[1], Author: item[2]}
+    posts = append(posts, post)
+  }
+
+  fmt.Println(posts[0].Id)
+  fmt.Println(posts[0].Content)
+  fmt.Println(posts[0].Author)
+
 }
