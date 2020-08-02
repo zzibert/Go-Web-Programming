e33dcb96da5e89139e563961f8457d26f0f42024
Zan Zibert <zan_zibert@hotmail.com>
2020-08-02 11:23:49 +0200

    one-to-many and many-to-one with Go

--- ChitChat/main.go
+++ ChitChat/main.go
@@ -1,89 +1,103 @@
 package main
 
 import (
-  "database/sql"
-  "fmt"
-  _ "github.com/lib/pq"
+	"database/sql"
+	"errors"
+	"fmt"
+
+	_ "github.com/lib/pq"
 )
 
 type Post struct {
-  Id int
-  Content string
-  Author string
+	Id       int
+	Content  string
+	Author   string
+	Comments []Comment
+}
+
+type Comment struct {
+	Id      int
+	Content string
+	Author  string
+	Post    *Post
 }
 
 var Db *sql.DB
 
 func init() {
-  var err error
+	var err error
 
-  Db, err = sql.Open("postgres", "user=zzibert dbname=postgres password=nekineki port=5432 sslmode=disable")
-  if err != nil {
-    panic(err)
-  }
+	Db, err = sql.Open("postgres", "user=zzibert dbname=postgres password=nekineki port=5432 sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
 }
 
 func Posts(limit int) (posts []Post, err error) {
-  rows, err := Db.Query("select id, content, author from posts limit $1", limit)
-  if err != nil {
-    return
-  }
-  for rows.Next() {
-    post := Post{}
-    err = rows.Scan(&post.Id, &post.Content, &post.Author)
-    if err != nil {
-      return
-    }
-    posts = append(posts, post)
-  }
-  rows.Close()
-  return
+	rows, err := Db.Query("select id, content, author from posts limit $1", limit)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		err = rows.Scan(&post.Id, &post.Content, &post.Author)
+		if err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
 }
 
 func GetPost(id int) (post Post, err error) {
-  post = Post{}
-  err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
+	post = Post{}
+	post.Comments = []Comment{}
+	err = Db.QueryRow("select id, content, author from posts where id = $1", id).Scan(&post.Id, &post.Content, &post.Author)
 
-  return
+	return
 }
 
 func (post *Post) Create() (err error) {
-  statement := "insert into posts (content, author) values ($1, $2) returning id"
-  stmt, err := Db.Prepare(statement)
-  if err != nil {
-    return
-  }
-  defer stmt.Close()
-  err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
-  return
+	statement := "insert into posts (content, author) values ($1, $2) returning id"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(post.Content, post.Author).Scan(&post.Id)
+	return
 }
 
 func (post *Post) Update() (err error) {
-  _, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
-  return
+	_, err = Db.Exec("update posts set content = $2, author = $3 where id = $1", post.Id, post.Content, post.Author)
+	return
 }
 
 func (post *Post) Delete() (err error) {
-  _, err = Db.Exec("delete from posts where id = $1", post.Id)
-  return
+	_, err = Db.Exec("delete from posts where id = $1", post.Id)
+	return
+}
+
+func (comment *Comment) Create() (err error) {
+	if comment.Post == nil {
+		err = errors.New("Post not found")
+		return
+	}
+	err = Db.QueryRow("insert into comments (content, author, post_id) values ($1, $2, $3) returning id", comment.Content, comment.Author, comment.Post.Id).Scan(&comment.Id)
+	return
 }
 
 func main() {
-  post := Post{Content: "Hello World!", Author: "Sau Sheong"}
+	post := Post{Content: "Hello World!", Author: "Sau Sheong"}
+	post.Create()
 
-  fmt.Println(post)
-  post.Create()
-  fmt.Println(post)
+	comment := Comment{Content: "Good post!", Author: "Joe", Post: &post}
+	comment.Create()
 
-  readPost, _ := GetPost(post.Id)
-  fmt.Println(readPost)
+	readPost, _ := GetPost(post.Id)
 
-  readPost.Content = "Bonjour Monde!"
-  readPost.Author = "Pierre"
-  readPost.Update()
-
-  posts, _ := Posts(1)
-  fmt.Println(posts)
-
-  readPost.Delete()
+	fmt.Println(readPost)
+	fmt.Println(readPost.Comments)
+	fmt.Println(readPost.Comments[0].Post)
 }
