a94ce08be2678be268a5a476fc24500d1636816f
Zan Zibert <zan_zibert@hotmail.com>
2020-08-02 18:17:18 +0200

    finishing chapter 7

--- ChitChat/docker/init.sql
+++ ChitChat/docker/init.sql
@@ -1,13 +1,13 @@
 
--- create table posts (
---   id serial primary key,
---   content text,
---   author varchar(255)
--- );
+create table posts (
+  id serial primary key,
+  content text,
+  author varchar(255)
+);
 
--- create table comments (
---   id serial primary key,
---   content text,
---   author varchar(255),
---   post_id integer references posts(id)
--- );
+create table comments (
+  id serial primary key,
+  content text,
+  author varchar(255),
+  post_id integer references posts(id)
+);
--- ChitChat/route_auth.go
+++ ChitChat/route_auth.go
@@ -1,24 +1,24 @@
-package main
+// package main
 
-import (
-	"net/http"
-  "github.com/zzibert/Go-Web-Programming/ChitChat/data"
-)
+// import (
+// 	"net/http"
+//   "github.com/zzibert/Go-Web-Programming/ChitChat/data"
+// )
 
-func authenticate(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
+// func authenticate(w http.ResponseWriter, r *http.Request) {
+// 	r.ParseForm()
 
-	user, _ := data.UserByEmail(r.PostFormValue("email"))
-	if user.Password == data.Encrypt(r.PostFormValue("password")) {
-		session := user.CreateSession()
-		cookie := http.Cookie{
-			Name:     "_cookie",
-			Value:    session.Uuid,
-			HttpOnly: true,
-		}
-		http.SetCookie(w, &cookie)
-		http.Redirect(w, r, "/", 302)
-	} else {
-		http.Redirect(w, r, "/login", 302)
-	}
-}
+// 	user, _ := data.UserByEmail(r.PostFormValue("email"))
+// 	if user.Password == data.Encrypt(r.PostFormValue("password")) {
+// 		session := user.CreateSession()
+// 		cookie := http.Cookie{
+// 			Name:     "_cookie",
+// 			Value:    session.Uuid,
+// 			HttpOnly: true,
+// 		}
+// 		http.SetCookie(w, &cookie)
+// 		http.Redirect(w, r, "/", 302)
+// 	} else {
+// 		http.Redirect(w, r, "/login", 302)
+// 	}
+// }
