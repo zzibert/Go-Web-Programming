2eadc2bee5c9a92b42459316e928eecc9ad40b0e
Zan Zibert <zan_zibert@hotmail.com>
2020-08-03 17:03:47 +0200

    Testing PUT requests

--- ChitChat/main/handleRequest_test.go
+++ ChitChat/main/handleRequest_test.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 )
 
@@ -23,5 +24,19 @@ func TestHandleGet(t *testing.T) {
 	json.Unmarshal(writer.Body.Bytes(), &post)
 	if post.Id != 1 {
 		t.Error("Cannot retrieve JSON post")
+	}
+}
+
+func TestHandlePut(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+
+	writer := httptest.NewRecorder()
+	json := strings.NewReader(`{"content":"Updated post","author":"Sau Sheong"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
 	}
 }
