46e52949b94496c7887728d24ec092cdc3c9c074
Zan Zibert <zan_zibert@hotmail.com>
2020-08-03 16:59:11 +0200

    testing GET request

---
+++ ChitChat/main/handleGet_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleGet(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/post/", handleRequest)
+
+	writer := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	var post Post
+	json.Unmarshal(writer.Body.Bytes(), &post)
+	if post.Id != 1 {
+		t.Error("Cannot retrieve JSON post")
+	}
+}
