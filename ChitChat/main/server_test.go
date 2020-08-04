69bfe4eba74e4faf75067c8e98e949444dd49f27
Zan Zibert <zan_zibert@hotmail.com>
2020-08-04 18:19:10 +0200

    fixtures using the check package

--- ChitChat/main/server_test.go
+++ ChitChat/main/server_test.go
@@ -4,12 +4,17 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	. "gopkg.in/check.v1"
 )
 
-type PostTestSuite struct{}
+type PostTestSuite struct {
+	mux    *http.ServeMux
+	post   *FakePost
+	writer *httptest.ResponseRecorder
+}
 
 func init() {
 	Suite(&PostTestSuite{})
@@ -17,15 +22,29 @@ func init() {
 
 func Test(t *testing.T) { TestingT(t) }
 
-func (s *PostTestSuite) TestHandleGet(c *C) {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
-	writer := httptest.NewRecorder()
-	request, _ := http.NewRequest("GET", "/post/1", nil)
-	mux.ServeHTTP(writer, request)
+func (s *PostTestSuite) SetUpTest(c *C) {
+	s.post = &FakePost{}
+	s.mux = http.NewServeMux()
+	s.mux.HandleFunc("/post/", handleRequest(s.post))
+	s.writer = httptest.NewRecorder()
+}
 
-	c.Check(writer.Code, Equals, 200)
+func (s *PostTestSuite) TestGetPost(c *C) {
+	request, _ := http.NewRequest("GET", "/post/1", nil)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
 	var post Post
-	json.Unmarshal(writer.Body.Bytes(), &post)
+	json.Unmarshal(s.writer.Body.Bytes(), &post)
 	c.Check(post.Id, Equals, 1)
+}
+
+func (s *PostTestSuite) TestPutPost(c *C) {
+	json := strings.NewReader(`{"content":"Updated post", "author":"Sau Sheong"}`)
+	request, _ := http.NewRequest("PUT", "/post/1", json)
+	s.mux.ServeHTTP(s.writer, request)
+
+	c.Check(s.writer.Code, Equals, 200)
+	c.Check(s.post.Id, Equals, 1)
+	c.Check(s.post.Content, Equals, "Updated post")
 }
