691f977b6de0761af21d45b31f24b2ed0f46082a
Zan Zibert <zan_zibert@hotmail.com>
2020-08-04 10:02:27 +0200

    the new handleRequest function

--- ChitChat/main/server.go
+++ ChitChat/main/server.go
@@ -26,26 +26,28 @@ func main() {
 	server := http.Server{
 		Addr: "127.0.0.1:8080",
 	}
-	http.HandleFunc("/post/", handleRequest)
+	http.HandleFunc("/post/", handleRequest())
 	server.ListenAndServe()
 }
 
-func handleRequest(w http.ResponseWriter, r *http.Request) {
-	var err error
+func handleRequest(t Text) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		var err error
 
-	switch r.Method {
-	case "GET":
-		err = handleGet(w, r)
-	case "POST":
-		err = handlePost(w, r)
-	case "PUT":
-		err = handlePut(w, r)
-	case "DELETE":
-		err = handleDelete(w, r)
-	}
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		switch r.Method {
+		case "GET":
+			err = handleGet(w, r, t)
+		case "POST":
+			err = handlePost(w, r, t)
+		case "PUT":
+			err = handlePut(w, r, t)
+		case "DELETE":
+			err = handleDelete(w, r, t)
+		}
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 	}
 }
 
