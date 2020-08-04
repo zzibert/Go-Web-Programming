f16f80e0d56324828c2370aef399e8ef6b3be754
Zan Zibert <zan_zibert@hotmail.com>
2020-08-04 12:25:38 +0200

    server_test.go using the check package

--- ChitChat/main/server_test.go
+++ ChitChat/main/server_test.go
@@ -4,47 +4,28 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"os"
-	"strings"
 	"testing"
+
+	. "gopkg.in/check.v1"
 )
 
-var mux *http.ServeMux
-var writer *httptest.ResponseRecorder
+type PostTestSuite struct{}
 
-func TestMain(m *testing.M) {
-	setUp()
-	code := m.Run()
-	os.Exit(code)
+func init() {
+	Suite(&PostTestSuite{})
 }
 
-func setUp() {
-	mux = http.NewServeMux()
+func Test(t *testing.T) { TestingT(t) }
+
+func (s *PostTestSuite) TestHandleGet(c *C) {
+	mux := http.NewServeMux()
 	mux.HandleFunc("/post/", handleRequest(&FakePost{}))
-	writer = httptest.NewRecorder()
-}
-
-func TestHandleGet(t *testing.T) {
+	writer := httptest.NewRecorder()
 	request, _ := http.NewRequest("GET", "/post/1", nil)
 	mux.ServeHTTP(writer, request)
 
-	if writer.Code != 200 {
-		t.Errorf("Response code is %v", writer.Code)
-	}
-
+	c.Check(writer.Code, Equals, 200)
 	var post Post
 	json.Unmarshal(writer.Body.Bytes(), &post)
-	if post.Id != 1 {
-		t.Errorf("Cannot retrieve JSON post")
-	}
-}
-
-func TestHandlePut(t *testing.T) {
-	json := strings.NewReader(`{"content":"Updated post", "author": "Sau Sheong"}`)
-	request, _ := http.NewRequest("PUT", "/post/1", json)
-	mux.ServeHTTP(writer, request)
-
-	if writer.Code != 200 {
-		t.Errorf("Response code is %v", writer.Code)
-	}
+	c.Check(post.Id, Equals, 1)
 }
