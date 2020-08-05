8e2b1b8ac89bb8a4b73603f5aa56306a9f6cd306
Zan Zibert <zan_zibert@hotmail.com>
2020-08-05 12:02:14 +0200

    passing messages with channels

--- simpleWebServer/goroutine.go
+++ simpleWebServer/goroutine.go
@@ -5,26 +5,23 @@ import (
 	"time"
 )
 
-func printNumbers2(w chan bool) {
-	for i := 0; i < 10; i++ {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%d ", i)
+func thrower(c chan int) {
+	for i := 0; i < 5; i++ {
+		c <- i
+		fmt.Println("Threw >>", i)
 	}
-	w <- true
 }
 
-func printLetters2(w chan bool) {
-	for i := 'A'; i < 'A'+10; i++ {
-		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%c ", i)
+func catcher(c chan int) {
+	for i := 0; i < 5; i++ {
+		num := <-c
+		fmt.Println("Caught <<", num)
 	}
-	w <- true
 }
 
 func main() {
-	w1, w2 := make(chan bool), make(chan bool)
-	go printNumbers2(w1)
-	go printLetters2(w2)
-	<-w1
-	<-w2
+	c := make(chan int)
+	go thrower(c)
+	go catcher(c)
+	time.Sleep(100 * time.Millisecond)
 }
