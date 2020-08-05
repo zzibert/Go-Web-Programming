32cac99d96bb140cf1cd784b43d187bedab3fa1a
Zan Zibert <zan_zibert@hotmail.com>
2020-08-05 10:36:33 +0200

    benchmarking function with and without goroutines

--- simpleWebServer/goroutine.go
+++ simpleWebServer/goroutine.go
@@ -14,24 +14,24 @@ func printNumbers1() {
 func printNumbers2() {
 	for i := 0; i < 10; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%d ", i)
+		// fmt.Printf("%d ", i)
 	}
 }
 
 func printLetters1() {
 	for i := 'A'; i < 'A'+10; i++ {
-		fmt.Printf("%c ", i)
+		// fmt.Printf("%c ", i)
 	}
 }
 
 func printLetters2() {
 	for i := 'A'; i < 'A'+10; i++ {
 		time.Sleep(1 * time.Microsecond)
-		fmt.Printf("%c ", i)
+		// fmt.Printf("%c ", i)
 	}
 }
 
-func print1() {
+func print2() {
 	printNumbers2()
 	printLetters2()
 }
--- simpleWebServer/goroutine_test.go
+++ simpleWebServer/goroutine_test.go
@@ -5,11 +5,19 @@ import (
 	"time"
 )
 
-func TestPrint1(t *testing.T) {
-	print1()
+func TestGoPrint2(t *testing.T) {
+	goPrint2()
+	time.Sleep(1 * time.Millisecond)
 }
 
-func TestGoPrint1(t *testing.T) {
-	goPrint1()
-	time.Sleep(1 * time.Millisecond)
+func BenchmarkPrint1(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		print2()
+	}
+}
+
+func BenchmarkGoPrint2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		goPrint2()
+	}
 }
